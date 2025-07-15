25ae0488424c0d3cc905d1acff91f97a55c02056
Umar Sajjad <47843546+umarsajjad1992@users.noreply.github.com>
2025-07-14 22:34:46 -0700

    Added coinChange function to the file coin_change.py

--- Dynamic_Programming/coin_change.py
+++ Dynamic_Programming/coin_change.py
@@ -1,3 +1,10 @@
-def coinChange(coins: list[int], amount: int) -> int:
+def coinChange(coins: list[int], amount: int):
     dp = [float('inf')] * (amount+1)
-    return 0
+    dp[0] = 0
+    for i in range(1, amount + 1):
+        for c in coins:
+            if i >= c:
+                dp[i] = min(dp[i], 1 + dp[i - c])
+    return dp[amount] if (dp[amount] != float('inf')) else -1
+
+print(coinChange([1,2,5], 11))
